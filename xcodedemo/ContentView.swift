f44eaca40403560865e10ffa35844e6b4418e61d
tilendra <tilendrachoudhary@gmail.com>
2024-08-18 11:00:19 -0400

    Initial Commit

---
+++ xcodedemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xcodedemo
+//
+//  Created by Tilendra Choudhary on 8/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xcodedemo/xcodedemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodedemoApp.swift
+//  xcodedemo
+//
+//  Created by Tilendra Choudhary on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct xcodedemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
